899e1fdd855bb2f05db1db9d0bc8c68ca7485205
Konstantin Shibkov <shibkov.k@gmail.com>
2021-12-22 18:07:54 +0500

    Initial commit

---
+++ src/main/java/practice/CardAccount.java
@@ -0,0 +1,6 @@
+package practice;
+
+public class CardAccount extends BankAccount {
+    // не забывайте, обращаться к методам и конструкторам родителя
+    // необходимо используя super, например, super.put(10D);
+}
---
+++ src/main/java/practice/DepositAccount.java
@@ -0,0 +1,5 @@
+package practice;
+
+public class DepositAccount extends BankAccount {
+
+}
